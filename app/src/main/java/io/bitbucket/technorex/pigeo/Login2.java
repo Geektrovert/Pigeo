15d669010eeab6d5fd8c74eeee0aeb1f84f285f1
sihan-tawsik <sihantawsik@gmail.com>
2018-10-20 19:59:08 +0600

    working on Google-SignIn

---
+++ app/src/main/java/io/bitbucket/technorex/pigeo/Login2.java
@@ -0,0 +1,26 @@
+package io.bitbucket.technorex.pigeo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class Login2 extends Activity {
+    GoogleSignInClient mGoogleSignInClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_login2);
+        super.onCreate(savedInstanceState);
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null){
+            startActivity(new Intent(this,MapsActivity.class));
+        }
+    }
+}
