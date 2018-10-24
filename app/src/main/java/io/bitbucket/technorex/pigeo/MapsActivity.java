655976da3541f0d29ffe2e49943f6c9d94293c82
sihan-tawsik <sihantawsik@gmail.com>
2018-10-24 20:28:08 +0600

    documentations added.

--- app/src/main/java/io/bitbucket/technorex/pigeo/MapsActivity.java
+++ app/src/main/java/io/bitbucket/technorex/pigeo/MapsActivity.java
@@ -24,6 +24,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
