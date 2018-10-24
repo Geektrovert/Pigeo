5e5b97578d2dca92f1f90ea8157337bf1341721e
sihan-tawsik <sihantawsik@gmail.com>
2018-10-24 20:24:50 +0600

    Google-Sign-In button function completed.(working) mapsActivity.kt showed illegal state Exception. replace with mapsActivity.java

---
+++ app/src/main/java/io/bitbucket/technorex/pigeo/LoginActivity.java
@@ -0,0 +1,78 @@
+package io.bitbucket.technorex.pigeo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends Activity {
+    private static int RC_SIGN_IN = 100;
+    private GoogleSignInClient mGoogleSignInClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_login);
+        super.onCreate(savedInstanceState);
+
+        //setting up google client
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        //checking for a previous logged in session
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        //if there is a previous session redirect to map activity
+        if(account!=null){
+            startActivity(new Intent(this,MapsActivity.class));
+        }
+
+        //customizing google sign in button
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        //adding listener to google sign in button
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleSignIn();
+            }
+        });
+    }
+    private void googleSignIn(){
+        Intent googleSignInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(googleSignInIntent,RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from mGoogleSignInClient.getSignInIntent(...);
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            startActivity(new Intent(this,MapsActivity.class));
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.e("ApiException", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(this,"signInResult:failed code=" + e.getStatusCode(),Toast.LENGTH_LONG).show();
+        }
+    }
+}
