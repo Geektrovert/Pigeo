f3aa4bf3c2c2480a2e2ea066208e6d7e73183175
sihan-tawsik <sihantawsik@gmail.com>
2018-11-06 21:23:52 +0600

    edited sign up layout files.

--- app/src/main/java/io/bitbucket/technorex/pigeo/activities/LoginActivity.java
+++ app/src/main/java/io/bitbucket/technorex/pigeo/activities/LoginActivity.java
@@ -132,7 +132,7 @@ public class LoginActivity extends Activity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(LoginActivity.this,SignUpActivity1.class));
             }
         });
 
