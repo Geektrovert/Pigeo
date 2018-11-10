b94f33b396b3b948b858ab1c4c413aafbe17f469
Tahsin Sayeed <sayeed0635@gmail.com>
2018-11-10 16:13:23 +0600

    Bottom nav bar problem gone!

--- app/src/main/java/io/bitbucket/technorex/pigeo/activities/MapsActivity.java
+++ app/src/main/java/io/bitbucket/technorex/pigeo/activities/MapsActivity.java
@@ -41,7 +41,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
-        //bindWidgets();
+        bindWidgets();
     }
 
     private void bindWidgets() {
