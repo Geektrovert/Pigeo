335ce20c3b40bb9985359e3928a7657b83aabaea
sihan-tawsik <sihantawsik@gmail.com>
2019-01-05 10:15:07 +0600

    labels added for edit texts in sign in1

--- app/src/main/java/io/bitbucket/technorex/pigeo/activities/SignUpActivity1.java
+++ app/src/main/java/io/bitbucket/technorex/pigeo/activities/SignUpActivity1.java
@@ -33,14 +33,34 @@ public class SignUpActivity1 extends Activity {
         nextPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(signUpPassword.getText().toString().equals(confirmPassword.getText().toString())) {
-                    String email = signUpEmail.getText().toString();
-                    String pass = signUpPassword.getText().toString();
-                    String[] EmailAndPassword = {email, pass};
-                    startActivity(new Intent(SignUpActivity1.this, SignUpActivity2.class).putExtra("EmailAndPass", EmailAndPassword));
+                if(validateInput(new EditText[]{signUpEmail,signUpPassword,confirmPassword})){
+                	if(signUpPassword.getText().toString().equals(confirmPassword.getText().toString())) {
+	                    String email = signUpEmail.getText().toString();
+	                    String pass = signUpPassword.getText().toString();
+	                    String[] EmailAndPassword = {email, pass};
+	                    startActivity(new Intent(SignUpActivity1.this, SignUpActivity2.class).putExtra("EmailAndPass", EmailAndPassword));
+	                }
+	                else{
+	                	setError(signUpPassword,R.string.passwords_dont_match);
+	                	Toast.makeText(SignUpActivity1.this, "Passwords don't match!", Toast.LENGTH_LONG).show();
+	                }
                 }
-                else Toast.makeText(SignUpActivity1.this, "Passwords don't match!", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private boolean validateInput(EditText[] editTexts) {
+        boolean flag = true;
+        for(EditText editText : editTexts) {
+            flag &= !editText.getText().toString().isEmpty();
+            if(editText.getText().toString().isEmpty()){
+                showError(editText,R.string.field_cant_be_empty);
+            }
+        }
+        return flag;
+    }
+
+    private void setError(EditText field, int messageRes){
+    	field.setError(getString(messageRes));
     }
 }
