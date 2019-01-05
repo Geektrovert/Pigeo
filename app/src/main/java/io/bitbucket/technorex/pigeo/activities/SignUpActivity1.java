badab657f4a8fde4101514479ae4a17b2dee0576
Geektrovert <namanush.rsr.16@gmail.com>
2019-01-05 09:57:44 +0600

    Sign Up activity and registration process - OnGoing (SignUpActivity1 completed)

--- app/src/main/java/io/bitbucket/technorex/pigeo/activities/SignUpActivity1.java
+++ app/src/main/java/io/bitbucket/technorex/pigeo/activities/SignUpActivity1.java
@@ -1,14 +1,46 @@
 package io.bitbucket.technorex.pigeo.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import io.bitbucket.technorex.pigeo.R;
 
 public class SignUpActivity1 extends Activity {
+
+    private EditText signUpEmail, signUpPassword, confirmPassword;
+    private Button nextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up1);
+        bindWidgets();
+        bindActivities();
+    }
+
+    private void bindWidgets() {
+        signUpEmail = findViewById(R.id.signUpEmail);
+        signUpPassword = findViewById(R.id.signUpPassword);
+        confirmPassword = findViewById(R.id.confirmPassword);
+        nextPage = findViewById(R.id.next);
+    }
+
+    private void bindActivities() {
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(signUpPassword.getText().toString().equals(confirmPassword.getText().toString())) {
+                    String email = signUpEmail.getText().toString();
+                    String pass = signUpPassword.getText().toString();
+                    String[] EmailAndPassword = {email, pass};
+                    startActivity(new Intent(SignUpActivity1.this, SignUpActivity2.class).putExtra("EmailAndPass", EmailAndPassword));
+                }
+                else Toast.makeText(SignUpActivity1.this, "Passwords don't match!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
