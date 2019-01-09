bbaff6e96ad754f5a00542c509bff291389eb5b6
sihan-tawsik <sihantawsik@gmail.com>
2019-01-09 19:55:13 +0600

    added permission asking(attempted)

--- app/src/main/java/io/bitbucket/technorex/pigeo/activities/StartActivity.java
+++ app/src/main/java/io/bitbucket/technorex/pigeo/activities/StartActivity.java
@@ -2,7 +2,10 @@ package io.bitbucket.technorex.pigeo.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import io.bitbucket.technorex.pigeo.R;
@@ -13,10 +16,28 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        checkPermissions();
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         if(firebaseUser != null)
             startActivity(new Intent(StartActivity.this, MapsActivity.class));
         else
             startActivity(new Intent(StartActivity.this, LoginActivity.class));
+    }
+
+    private void checkPermissions() {
+        String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.INTERNET,android.Manifest.permission.READ_EXTERNAL_STORAGE,android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.READ_CONTACTS};
+        String[] permission1 = {android.Manifest.permission.READ_CONTACTS};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for(String permission : permission1){
+                if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, permission)!= PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{permission}, 3);
+                }
+                else{
+                    //requestPermissions(new String[]{permission},10);
+                }
+            }
+        }
     }
 }
