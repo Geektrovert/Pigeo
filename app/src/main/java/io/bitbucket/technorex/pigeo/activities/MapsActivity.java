2a10c59ffba4bcdc3c1a2310445cfebb5aa3f12c
Geektrovert <namanush.rsr.16@gmail.com>
2019-01-12 14:30:15 +0600

    :rocket: Stable version 0.1

--- app/src/main/java/io/bitbucket/technorex/pigeo/activities/MapsActivity.java
+++ app/src/main/java/io/bitbucket/technorex/pigeo/activities/MapsActivity.java
@@ -13,7 +13,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
