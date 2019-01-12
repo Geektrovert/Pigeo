4b9380d6d5333855baf48e96d62d81cd9273882d
sihan-tawsik <sihantawsik@gmail.com>
2019-01-12 19:51:48 +0600

    :rocket: recover password successful

--- app/src/main/java/io/bitbucket/technorex/pigeo/activities/LoginActivity.java
+++ app/src/main/java/io/bitbucket/technorex/pigeo/activities/LoginActivity.java
@@ -90,7 +90,7 @@ public class LoginActivity extends Activity {
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(LoginActivity.this,RetrievePassword.class));
             }
         });
 
